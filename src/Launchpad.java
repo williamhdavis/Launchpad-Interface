620993e307c2010d4fc9732a6f12e69d7264ea6e
williamhdavis <williamhdavis@users.noreply.github.com>
2018-08-18 00:41:33 +0100

    Addition of menu key colour setting.

--- src/Launchpad.java
+++ src/Launchpad.java
@@ -5,30 +5,79 @@ import javax.sound.midi.*;
 
 public class Launchpad
 {
+    /**
+     * The NONE class variable defines the value to be used to set an led to off.
+     */
     public static final int NONE = 0;
+    /**
+     * The LOW class variable defines the value to be used to set an led to low brightness.
+     */
     public static final int LOW = 1;
+    /**
+     * The MID class variable defines the value to be used to set an led to medium brightness.
+     */
     public static final int MID = 2;
+    /**
+     * The HIGH class variable defines the value to be used to set an led to maximum brightness.
+     */
     public static final int HIGH = 3;
 
-    public static final boolean SOLID = false;
-    public static final boolean FLASHING = true;
-
-    private static final int GRID_WIDTH = 8;
-    private static final int GRID_HEIGHT = 8;
+    /**
+     * The GRID_SIZE class variable is used to define the width and height of the square button grid on the launchpad.
+     */
+    private static final int GRID_SIZE = 8;
+    /**
+     * The GRID_MULTIPLIER class variable is used to define the change in key number on the y-axis of the launchpad.
+     */
     private static final int GRID_MULTIPLIER = 16;
+    /**
+     * The GRID_TOP_ROW_OFFSET class variable is used to define the offset from 0 of the top keys address.
+     */
+    private static final int GRID_TOP_ROW_OFFSET  = 104;
+    /**
+     * The GRID_SIDE_ROW_OFFSET class variable is used to define the offset from 0 of the side keys address.
+     */
+    private static final int GRID_SIDE_ROW_OFFSET = 8;
 
+    /**
+     * The BASE class variable defines the base value for any solid colour.
+     */
     private static final int BASE = 12;
+    /**
+     * The BASE_FLASHING class variable defines the base value for any flashing colour.
+     */
     private static final int BASE_FLASHING = 8;
-    private static final int MULTIPLIER_RED = 1;
+
+    /**
+     * The MULTIPLIER_GREEN class variable is used to define the multiplier required for green colour values.
+     */
     private static final int MULTIPLIER_GREEN = 16;
 
+    /**
+     * The CONTROLLER_GRID class variable is used to define the controller to use for accessing the grid.
+     */
     private static final int CONTROLLER_GRID = 144;
+    /**
+     * The CONTROLLER_MASTER class variable is used to define the controller for the menu keys and special functions.
+     */
     private static final int CONTROLLER_MASTER = 176;
 
+    /**
+     * The KEY_MASTER class variable is used to define the key code for special commands.
+     */
     private static final int KEY_MASTER = 0;
 
+    /**
+     * The VALUE_RESET class variable is used to define the value sent to the launchpad to clear all keys.
+     */
     private static final int VALUE_RESET = 0;
+    /**
+     * The VALUE_DEFAULT class variable is used to define the value sent to the launchpad to put it in default mode.
+     */
     private static final int VALUE_DEFAULT = 32;
+    /**
+     * The VALUE_FLASHING class variable is used to define the value sent to the launchpad to enable flashing buttons.
+     */
     private static final int VALUE_FLASHING = 40;
 
     private boolean valid;
@@ -108,7 +157,7 @@ public class Launchpad
 
     public int createColour(int red, int green)
     {
-        return this.createColour(red, green, SOLID);
+        return this.createColour(red, green, false);
     }
 
     public int createColour(int red, int green, boolean flashing)
@@ -126,7 +175,7 @@ public class Launchpad
         {
             sum += BASE;
         }
-        sum += MULTIPLIER_RED * red + MULTIPLIER_GREEN * green;
+        sum += red + MULTIPLIER_GREEN * green;
         return sum;
     }
 
@@ -145,9 +194,9 @@ public class Launchpad
 
     public void setGridKey(int x, int y, int colour)
     {
-        if(0 <= x && x < GRID_WIDTH)
+        if(0 <= x && x < GRID_SIZE)
         {
-            if(0 <= y && y < GRID_HEIGHT)
+            if(0 <= y && y < GRID_SIZE)
             {
                 try
                 {
@@ -161,12 +210,32 @@ public class Launchpad
 
     public void setGridKey(int x, int y, int red, int green)
     {
-        this.setGridKey(x, y, this.createColour(red, green, SOLID));
+        this.setGridKey(x, y, this.createColour(red, green, false));
     }
 
     public void setGridKey(int x, int y, int red, int green, boolean flashing)
     {
         this.setGridKey(x, y, this.createColour(red, green, flashing));
+    }
+
+    public void setMenuKey(boolean topRow, int button, int colour)
+    {
+        if(0 <= button && button < GRID_SIZE)
+        {
+            try
+            {
+                if(topRow)
+                {
+                    this.sendMessage(CONTROLLER_MASTER, GRID_TOP_ROW_OFFSET + button, colour);
+                }
+                else
+                {
+                    this.sendMessage(CONTROLLER_GRID, GRID_MULTIPLIER * button + GRID_SIDE_ROW_OFFSET, colour);
+                }
+            }
+            catch(InvalidMidiDataException ex)
+            {}
+        }
     }
 
     private void sendMessage(int controller, int key, int value) throws InvalidMidiDataException
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // DEBUGGING CODE
         try
         {
             Launchpad launchpad = new Launchpad();
@@ -12,22 +13,40 @@ public class Main
             launchpad.setGridKey(0, 0, launchpad.createColour(Launchpad.NONE, Launchpad.NONE));
             launchpad.setGridKey(1, 0, launchpad.createColour(Launchpad.LOW, Launchpad.NONE));
             launchpad.setGridKey(2, 0, launchpad.createColour(Launchpad.MID, Launchpad.NONE));
-            launchpad.setGridKey(3, 0, launchpad.createColour(Launchpad.HIGH, Launchpad.NONE, Launchpad.FLASHING));
+            launchpad.setGridKey(3, 0, launchpad.createColour(Launchpad.HIGH, Launchpad.NONE, true));
 
-            launchpad.setGridKey(0, 1, launchpad.createColour(Launchpad.NONE, Launchpad.LOW, Launchpad.SOLID));
-            launchpad.setGridKey(1, 1, launchpad.createColour(Launchpad.LOW, Launchpad.LOW, Launchpad.SOLID));
-            launchpad.setGridKey(2, 1, launchpad.createColour(Launchpad.MID, Launchpad.LOW, Launchpad.SOLID));
-            launchpad.setGridKey(3, 1, launchpad.createColour(Launchpad.HIGH, Launchpad.LOW, Launchpad.SOLID));
+            launchpad.setGridKey(0, 1, launchpad.createColour(Launchpad.NONE, Launchpad.LOW));
+            launchpad.setGridKey(1, 1, launchpad.createColour(Launchpad.LOW, Launchpad.LOW));
+            launchpad.setGridKey(2, 1, launchpad.createColour(Launchpad.MID, Launchpad.LOW));
+            launchpad.setGridKey(3, 1, launchpad.createColour(Launchpad.HIGH, Launchpad.LOW));
 
-            launchpad.setGridKey(0, 2, launchpad.createColour(Launchpad.NONE, Launchpad.MID, Launchpad.SOLID));
-            launchpad.setGridKey(1, 2, launchpad.createColour(Launchpad.LOW, Launchpad.MID, Launchpad.SOLID));
-            launchpad.setGridKey(2, 2, launchpad.createColour(Launchpad.MID, Launchpad.MID, Launchpad.SOLID));
-            launchpad.setGridKey(3, 2, launchpad.createColour(Launchpad.HIGH, Launchpad.MID, Launchpad.SOLID));
+            launchpad.setGridKey(0, 2, launchpad.createColour(Launchpad.NONE, Launchpad.MID));
+            launchpad.setGridKey(1, 2, launchpad.createColour(Launchpad.LOW, Launchpad.MID));
+            launchpad.setGridKey(2, 2, launchpad.createColour(Launchpad.MID, Launchpad.MID));
+            launchpad.setGridKey(3, 2, launchpad.createColour(Launchpad.HIGH, Launchpad.MID));
 
-            launchpad.setGridKey(0, 3, launchpad.createColour(Launchpad.NONE, Launchpad.HIGH, Launchpad.FLASHING));
-            launchpad.setGridKey(1, 3, launchpad.createColour(Launchpad.LOW, Launchpad.HIGH, Launchpad.SOLID));
-            launchpad.setGridKey(2, 3, launchpad.createColour(Launchpad.MID, Launchpad.HIGH, Launchpad.SOLID));
-            launchpad.setGridKey(3, 3, launchpad.createColour(Launchpad.HIGH, Launchpad.HIGH, Launchpad.FLASHING));
+            launchpad.setGridKey(0, 3, launchpad.createColour(Launchpad.NONE, Launchpad.HIGH, true));
+            launchpad.setGridKey(1, 3, launchpad.createColour(Launchpad.LOW, Launchpad.HIGH));
+            launchpad.setGridKey(2, 3, launchpad.createColour(Launchpad.MID, Launchpad.HIGH));
+            launchpad.setGridKey(3, 3, launchpad.createColour(Launchpad.HIGH, Launchpad.HIGH, true));
+
+            launchpad.setMenuKey(true, 0, 60);
+            launchpad.setMenuKey(true, 1, 60);
+            launchpad.setMenuKey(true, 2, 60);
+            launchpad.setMenuKey(true, 3, 60);
+            launchpad.setMenuKey(true, 4, 60);
+            launchpad.setMenuKey(true, 5, 60);
+            launchpad.setMenuKey(true, 6, 60);
+            launchpad.setMenuKey(true, 7, 60);
+
+            launchpad.setMenuKey(false, 0, 60);
+            launchpad.setMenuKey(false, 1, 60);
+            launchpad.setMenuKey(false, 2, 60);
+            launchpad.setMenuKey(false, 3, 60);
+            launchpad.setMenuKey(false, 4, 60);
+            launchpad.setMenuKey(false, 5, 60);
+            launchpad.setMenuKey(false, 6, 60);
+            launchpad.setMenuKey(false, 7, 60);
 
             Thread.sleep(5000);
             launchpad.reset();
